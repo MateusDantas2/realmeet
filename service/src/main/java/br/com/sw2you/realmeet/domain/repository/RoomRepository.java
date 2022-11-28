9eeb8520dfaa2d56948d3ca7c2f3fd5155b2f95e
Mateus Dantas <mateusdantas.dev@gmail.com>
2022-11-27 21:43:35 -0300

    Cria metodo update no RoomRepository

--- service/src/main/java/br/com/sw2you/realmeet/domain/repository/RoomRepository.java
+++ service/src/main/java/br/com/sw2you/realmeet/domain/repository/RoomRepository.java
@@ -17,4 +17,8 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("UPDATE Room r SET r.active = false WHERE r.id = :roomId")
     void deactivate(@Param("roomId") Long roomId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE Room r SET r.name = :name, r.seats = :seats WHERE r.id = :roomId")
+    void updateRoom(@Param("roomId") Long roomId, @Param("name") String name, @Param("seats") Integer seats);
 }
